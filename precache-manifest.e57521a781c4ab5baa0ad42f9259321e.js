df300b27e0464de3b0738f43d93d139dfe85139f
Artem <temaqwerty15@gmail.com>
2021-07-15 18:09:19 +0300

    Updates

---
+++ precache-manifest.e57521a781c4ab5baa0ad42f9259321e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0a33e7a588325a21ae423fada4744d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd22ddb8cb0f935dc434",
+    "url": "./static/css/main.c7f92f48.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd22ddb8cb0f935dc434",
+    "url": "./static/js/main.9536fd4b.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
